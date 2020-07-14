5a0be75915c417203740cb411a022417bb6a0d09
Caroline Avellar <carolineavellar@outlook.com>
2020-07-13 22:27:12 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link href='https://api.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.css' rel='stylesheet' />  
+  <link rel="stylesheet" href="public/stylesheets/main.css">
+  <title>Teste - Tela Mobile</title>
+</head>
+
+<body>
+  <div class="container">
+    <?php include './src/partials/map.php';?>
+    <?php include './src/partials/infos.php';?>
+  </div>
+  <script src='https://api.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.js'></script>
+</body>
+
+</html>
